c5b858f6d8e57351075df0afaebd63231a31e2a7
joao <joaogabriel130206522@gmail.com>
2023-08-31 15:27:44 -0300

    Exercicio02

---
+++ Script.js
@@ -0,0 +1,3 @@
+//Exercicio proposto no curso faca um script tendo um objeto que represente um Fiar uno, sendo necessario ter como informacoes 
+//ano, modelo, se estar em linha de producao  e pelo menos tres cores
+let carroUno = { Ano: '1990', modelo: 'FiatunoMile', EmProducao: false, cor: ['branco', 'vermelho', 'azul', 'verde'] };
